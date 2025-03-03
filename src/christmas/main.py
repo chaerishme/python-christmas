8c7f6621f88bf913d91163410274ddba71340292
chaerishme <sbk93@naver.com>
2025-03-03 23:06:07 +0900

    [#2] 주문 메뉴 출력까지 구현

--- src/christmas/main.py
+++ src/christmas/main.py
@@ -1,8 +1,17 @@
 from input_view import InputView
+from output_view import OutputView
+
 
 def main():
-    # TODO: 프로그램 구현
-    pass
+    input_view = InputView()
+    output_view = OutputView()
+
+    print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
+    date = input_view.read_date()
+    orders = input_view.read_order()
+    print(f"12월 {date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!")
+
+    output_view.print_menu(orders)
 
 if __name__ == "__main__":
     main()
