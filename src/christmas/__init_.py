5da0378cfdd2e230078d0ddb31a83fea635cb12c
chaerishme <sbk93@naver.com>
2025-03-04 21:00:43 +0900

    [#2] PEP8 따라 수정

--- src/christmas/__init_.py
+++ src/christmas/__init_.py
@@ -3,6 +3,6 @@
 """
 from .input_view import InputView
 from .output_view import OutputView
-from .calculate import Calculate 
+from .calculate import Calculate
 
 __all__ = ["InputView", "OutputView", "Calculate"]
